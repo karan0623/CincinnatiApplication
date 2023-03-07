1a31894954eaa2e8eebf618344f8aa53edb9daa6
karan0623 <64561330+karan0623@users.noreply.github.com>
2023-03-07 11:57:40 -0500

    Add project files.

---
+++ CincinnatiApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CincinnatiApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
